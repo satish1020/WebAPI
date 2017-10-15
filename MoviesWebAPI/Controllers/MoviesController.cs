9dc823a5586aafa9a7c5b370833c2a359acb6b41
Venkata Satish <cvsatish.net@gmail.com>
2017-10-15 11:50:22 -0500

    Commiting The Code

---
+++ MoviesWebAPI/Controllers/MoviesController.cs
@@ -0,0 +1,37 @@
+﻿using MoviesDb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MoviesDb.Controllers
+{
+    public class MoviesController : ApiController
+    {
+        Movie[] movies = new Movie[]
+       {
+            new Movie { Id = 1, Name = "Titanic", Genre = "Drama", Price = 9 },
+            new Movie { Id = 2, Name = "Rocky", Genre = "Drama", Price = 5.75M },
+            new Movie { Id = 3, Name = "Team America", Genre = "Comedy", Price = 8.99M }
+       };
+       
+        public IEnumerable<Movie> GetAllMovies()
+        {
+            return movies;
+        }
+
+       
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = movies.FirstOrDefault((p) => p.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return Ok(movie);
+        }
+
+    }
+}
---
+++ MoviesWebAPI/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoviesDb.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
